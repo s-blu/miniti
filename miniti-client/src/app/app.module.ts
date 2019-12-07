9367158e6928d84e0bbef8ba2f889fdd718e4c48
Stefanie S <sts@s-blu.de>
2019-12-07 11:11:57 +0100

    test(#5): add primeNG to project

--- miniti-client/src/app/app.module.ts
+++ miniti-client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +17,7 @@ import { TicketDetailsComponent } from './ticket-details/ticket-details.componen
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
