30590c1ec0a99d6b05fc1c49ac11f4488a37c833
Stefanie S <sts@s-blu.de>
2019-12-08 08:57:29 +0100

    feat(#5): add primeng to project

--- miniti-client/src/app/app.module.ts
+++ miniti-client/src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TicketListComponent } from './ticket-list/ticket-list.component';
-import { TicketEntryComponent } from './ticket-entry/ticket-entry.component';
-import { TicketDetailsComponent } from './ticket-details/ticket-details.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {TicketListComponent} from './ticket-list/ticket-list.component';
+import {TicketEntryComponent} from './ticket-entry/ticket-entry.component';
+import {TicketDetailsComponent} from './ticket-details/ticket-details.component';
+
 
 @NgModule({
   declarations: [
@@ -23,4 +24,5 @@ import { TicketDetailsComponent } from './ticket-details/ticket-details.componen
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
